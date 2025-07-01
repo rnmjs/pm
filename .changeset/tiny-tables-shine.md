4e0f6a92c78014b2614e2922eacd701e6ac12738
zanminkian <hellozmj@qq.com>
2025-07-01 07:23:57 +0000

    feat: add `px` command

---
+++ .changeset/tiny-tables-shine.md
@@ -0,0 +1,5 @@
+---
+"@rnm/pm": patch
+---
+
+feat: add `px` command
---
+++ packages/pm/src/px.cli.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import process from "node:process";
+import { detect, getMsg, run } from "./base.ts";
+
+const result = await detect();
+
+const args = [
+  ...(result?.name === "pnpm" || result?.name === "yarn"
+    ? ["dlx"]
+    : ["exec", "--"]),
+  ...process.argv.slice(2),
+];
+const msg = getMsg(result, args);
+console.log(msg);
+await run(result, args);
