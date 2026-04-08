09330ca8ca6aae7708fbfac0224a88b781564732
zanminkian <hellozmj@qq.com>
2026-04-08 19:16:47 +0000

    chore: upgrade deps

--- packages/pm/src/common.ts
+++ packages/pm/src/common.ts
@@ -1,3 +1,4 @@
+// TODO: Use [module.findPackageJSON](https://nodejs.org/api/module.html#modulefindpackagejsonspecifier-base) when dropping support for Node.js < 22.14.0.
 // eslint-disable-next-line esm/no-external-src-imports -- JSON import needed to read package.json at runtime
 import packageJson from "../package.json" with { type: "json" };
 
